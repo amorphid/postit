5c970addf2a9a8026fbbfdcbf4967f4660a4f619
Michael Pope <amorphid@captaincoder.io>
2013-08-10 13:34:26 -0700

    Create comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 PostitTemplate::Application.routes.draw do
-  resources :posts, except: :destroy
+  resources :posts, except: :destroy do
+    resources :comments, only: :create
+  end
 
   root to: 'posts#index'
 end
