5cf30c08473dc400c264e6436fdef5bd9e9904f0
Michael Pope <amorphid@captaincoder.io>
2013-08-10 13:28:56 -0700

    Add methods to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,19 @@
 class PostsController < ApplicationController
-  def index
+  def create
+  end
 
+  def edit
+  end
+
+  def update
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def update
   end
 end
