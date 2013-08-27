7dd72f6f25243a4b3915d0d654ae85ad8a6142f1
Michael Pope <amorphid@captaincoder.io>
2013-08-26 22:01:58 -0700

    Votes working in posts index

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,19 +1,34 @@
 class VotesController < ApplicationController
 	before_filter :authorize
 
-	def create
-		vote = Vote.new(value: params[:value])
-		vote.votable = set_votable
-		vote.user = current_user
-		vote.save
+  def create
+    @vote = find_or_initialize_vote
+
+		case
+    when identical? then @vote.destroy
+    when opposite?  then @vote.update_attributes(value: params[:value])
+    else
+      @vote.value = params[:value]
+      @vote.save
+    end
+
 		redirect_to :back
 	end
 
 private
 
-	def set_votable  
+  def identical?
+    @vote.value == params[:value].to_i
+  end
+
+  def opposite?
+    @vote.value == -params[:value].to_i
+  end
+
+	def find_or_initialize_vote
 		if params.has_key? :post_id
-			Post.find(params[:post_id])
+      post = Post.find(params[:post_id])
+			Vote.find_or_initialize_by(votable: post, user_id: current_user.id)
 		end
 	end
 end
